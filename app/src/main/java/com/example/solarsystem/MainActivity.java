0b51eae55aafa79f75f9c96ed145c07fe37283a0
Nowocinski <vito24222@gmail.com>
2022-06-03 18:37:37 +0200

    Improve colors

--- app/src/main/java/com/example/solarsystem/MainActivity.java
+++ app/src/main/java/com/example/solarsystem/MainActivity.java
@@ -46,14 +46,7 @@ public class MainActivity extends AppCompatActivity implements MoonsFragment.ITa
 
         this.moonsTabLayout = binding.appBarMain.moonsTabLayout;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
+    
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
